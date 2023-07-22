d63507464dd0cc56ce9912e946b5c958e6f9241a
jephthah-snr <jsnr300@gmail.com>
2023-07-22 14:03:42 +0100

    added cart table schema

--- cart_table.ts
+++ cart_table.ts
@@ -1,18 +1,19 @@
 import { Knex } from "knex";
-
-const tableName = ""; //table name goes here
+const tableName = "cart";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable(tableName, async (table) => {
-    table.uuid("id").notNullable().primary().defaultTo(knex.raw("uuid_generate_v4()")); //db exension will be installed to postgres to auto generate the uuid
-    table.string("value").notNullable();
-    table.boolean("is_verified").defaultTo(true)
-    table.enum("name", ["value1", "value2"])
-    table.foreign('productId').references('products.id').onUpdate('CASCADE')
-    table.timestamps(true, true);
-  });
+    return knex.schema.createTable(tableName, (table: Knex.TableBuilder) => {
+        table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()')); //since were using postgres
+        table.uuid("userId").notNullable();
+        table.uuid("productId");
+        table.integer("quantity");
+        table.foreign("userId").references('users.id').onUpdate('CASCADE')  //.onDelete()?
+        table.foreign("productId").references('products.id').onUpdate('CASCADE')
+        table.timestamps(true, true);
+    })
 }
 
+
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTableIfExists(tableName);
+    return knex.schema.dropTableIfExists(tableName)
 }
