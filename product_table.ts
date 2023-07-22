3a75670a4721d685db286cbd349e72e9e177221c
JamiuKolawole04 <Jakko4real@gmail.com>
2023-07-22 15:20:32 +0100

    chore: modifying product schema

--- product_table.ts
+++ product_table.ts
@@ -3,21 +3,22 @@ import { Knex } from "knex";
 const tableName = "products";
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable(tableName, (table: Knex.TableBuilder) => {
-        table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
-        table.string("name");
-        table.string("amount");
-        table.text("description");
-        table.text("bannerImage");
-        table.text("images");
-        table.integer("quantity");
-        table.boolean("outOfStock").defaultTo(false)
-        table.timestamps(true, true);
-    })
+  return knex.schema.createTable(tableName, (table: Knex.TableBuilder) => {
+    table.uuid("id").primary().defaultTo(knex.raw("uuid_generate_v4()"));
+    table.string("name");
+    table.string("amount");
+    table.text("description");
+    table.text("bannerImage");
+    table.text("images");
+    table.integer("quantity");
+    table.integer("discountPercentage");
+    table.integer("discountPrice");
+    table.tinyint("rating");
+    table.boolean("outOfStock").defaultTo(false);
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTableIfExists(tableName)
+  return knex.schema.dropTableIfExists(tableName);
 }
-
